5b3e9e302ca5d8af0c897c928554438c6ab7ede2
Le Chi <le.khanhchi@yahoo.fr>
2021-02-24 21:18:07 +0100

    ajouter les routes de validation formulaire et securite

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,15 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 // pour une protection api
-const helmet = require('helmet')
+const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit'); 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100  // limit each IP to 100 requests per windowMs
+});
 
 const app = express();
-app.use(helmet());
 
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
@@ -26,6 +31,10 @@ app.use((req, res, next) => {
 })
 
 app.use(bodyParser.json());
+
+app.use(helmet());
+app.use(cors({origin: 'http://localhost:4200'}));
+app.use(limiter);
 
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+const ExpressBrute = require('express-brute'); // contre attaque force-brute
+const store = new ExpressBrute.MemoryStore(); //===> Stores state locally, don't use this in production
+const bruteforce = new ExpressBrute(store);
+
 const userCtrl = require('../controller/user');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+const bodyParser = require('body-parser');// validator pour formulaire login
+const jsonParser = bodyParser.json();
+const {check, valideResult} = require('express-validator'); 
+router.post('/signup', jsonParser, [
+    check ('email', 'Email is not valid')
+        .isEmail()
+        .bail()
+        .normalizeEmail(),
+    check('password', 'Password must be +8 characters long')
+        .isLength({min: 8})
+        .isLowercase({min:1})
+        .isUppercase({min: 1})
+        .isStrongPassword()
+        
+], (req, res) => {
+    const errors = valideResult(req)
+    if(!errors.isEmpty()) { 
+        const alert = errors.array();
+        res.render('/signup', {alert})
+    }
+    else{ userCtrl.signup}
+})
+
+// router.post('/signup', userCtrl.signup);
+router.post('/login', bruteforce.prevent, userCtrl.login);
 
 module.exports = router
